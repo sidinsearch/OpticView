9d28ffb9d3e0b178613eae077be44e9b3d161235
Prathamesh0901 <prathameshmane3622@gmail.com>
2025-04-13 11:48:37 +0530

    removed web version

--- Web Version/capture.py
+++
@@ -1,8 +0,0 @@
-import base64
-import re
-
-def save_image_from_base64(b64_data, filename="static/capture.jpg"):
-    b64_data = re.sub('^data:image/.+;base64,', '', b64_data)
-    with open(filename, "wb") as f:
-        f.write(base64.b64decode(b64_data))
-    return filename
